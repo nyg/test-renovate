2e015c203deb5eba2495d30dd93ac29027bcaf5c
nyg <nyg@users.noreply.github.com>
2022-02-19 17:40:21 +0100

    Add pom.xml and package.json

---
+++ src/main/java/ch/nyg/java/test/App.java
@@ -0,0 +1,13 @@
+package ch.nyg.java.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
